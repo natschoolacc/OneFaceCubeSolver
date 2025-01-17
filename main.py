71caa31926ca3eec8d6a0ac1864d47f2e57cf234
natschoolacc <nossonpitterman@azyeshiva.org>
2025-01-17 13:57:14 -0700

    welcome msg

---
+++ main.py
@@ -0,0 +1,3 @@
+print("finds a possible solution to 5 sides of the rubix cube givin only one side:\n")
+print("data should be givin left to right top to bottom")
+print("exemple: w w w\n w w w\n w w w")
