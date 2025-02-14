0ab67808b62a452ee8612d11d3ed3f9fb2cd051b
natschoolacc <nossonpitterman@azyeshiva.org>
2025-02-14 20:31:16 +0000

    dd2

---
+++ algorithm.py
@@ -0,0 +1,6 @@
+rows = []
+def rowInp(rows):
+
+    
+
+    #https://prod.liveshare.vsengsaas.visualstudio.com/join?EB46972A1ACB3E474331CF057F221C7C4BB6
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import algorithm.py
+
 print("finds a possible solution to 5 sides of the rubix cube givin only one side:\n")
 print("data should be givin left to right top to bottom")
 print("exemple: w w w\n w w w\n w w w")
-
+row1 = input("Row 1: ")
+row2 = input("Row 2: ")
+row3 = input("Row 3: ")
+rows = [row1, row2, row3]
 ##https://prod.liveshare.vsengsaas.visualstudio.com/join?73111D919D8EB78D50F661317054F7746B6D
+
+
+rowInp(rows)
